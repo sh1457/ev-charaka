c742f802e3fec7ab374b37f8e0304ac82370ba10
Sujith Sudarshan <sh1457@gmail.com>
2023-01-28 21:18:08 +0530

    Add entities module

---
+++ src/evcharaka/entity.py
@@ -0,0 +1,192 @@
+from __future__ import annotations
+from dataclasses import dataclass
+from pathlib import Path
+import json
+
+import pandas as pd
+
+from evcharaka.util import hours_human_readable
+
+@dataclass
+class Entity:
+    @classmethod
+    def load(cls, key: str) -> Entity:
+        search_key_field = cls.__dataclass_fields__.get("search_key", None)
+
+        if search_key_field is None:
+            raise ValueError(f"No search key implemented for {cls.__name__!s}")
+
+        if not key:
+            raise ValueError(f"No search key provided for {cls.__name__!s}")
+
+        search_terms = [x.strip('{}') for x in search_key_field.default.split(' ')]
+
+        key = key.lower().rsplit(' ', maxsplit=len(search_terms) - 1)
+
+        q = ' and '.join([f"{k}.astype('str').str.lower().str.contains('{v}')" for k, v in zip(search_terms, key)])
+
+        cwd = Path.cwd()
+        while cwd.name != "ev-charaka":
+            cwd = cwd.parent
+
+        entity_path = f"{cls.__name__.lower()}s.csv"
+        path = cwd / "data" / entity_path
+
+        df = None
+        try:
+            with open(path) as fp:
+                df = pd.read_csv(fp).query(q, engine='python')
+
+        except Exception as exc:
+            print(exc)
+
+        length = df.shape[0]
+
+        if length == 0:
+            raise ValueError("No search results")
+
+        if length > 1:
+            print(df.values)
+            raise ValueError(f"Ambigous search term for search key: {search_key_field.default}")
+
+        obj = cls(**df.iloc[0].to_dict())
+
+        return obj
+
+
+@dataclass
+class Car(Entity):
+    make: str
+    model: str
+    variant: str
+    capacity: float
+    max_range: float
+    exp_range: float
+    slow_charge_rate: float
+    fast_charge_rate: float
+    search_key: str = "{model}"
+
+    @classmethod
+    def load(cls, key: str) -> Car:
+        return super().load(key)
+
+
+@dataclass
+class Charger(Entity):
+    type: str
+    charge_rate: float
+    search_key: str = "{type} {charge_rate}"
+
+    @classmethod
+    def load(cls, key: str) -> Charger:
+        return super().load(key)
+
+
+@dataclass
+class DriveParams:
+    avg_speed: float
+    avg_energy_consumption: float
+    daily_start_time: int
+
+
+@dataclass
+class Leg:
+    name: str
+    gmaps_link: str
+    waypoints: list[Waypoint]
+
+    def __str__(self) -> str:
+        text = f"{self.name}\n"
+        for wp in self.waypoints:
+            text += f"|-- {wp!s}"
+
+        text += f"{'- ' * 44}\n"
+        text += f"|-- {' ' * 9} | {self.distance: >7.2f} km | {hours_human_readable(self.duration)} |\n"
+        text += f"{'- ' * 44}\n"
+        return text
+
+    @property
+    def distance(self) -> float:
+        return sum(map(lambda wp: wp.distance, self.waypoints[:-1]))
+
+    @property
+    def duration(self) -> int:
+        return sum(map(lambda wp: wp.duration, self.waypoints[:-1]))
+
+
+@dataclass
+class Trip:
+    name: str
+    legs: list[Leg]
+
+    @classmethod
+    def load_trip(cls, path: Path) -> Trip:
+        legs = []
+        with open(path, "r") as fp:
+            for line in fp.readlines():
+                leg_data = json.loads(line)
+                waypoints = []
+                for wp in leg_data["waypoints"]:
+                    try:
+                        waypoints.append(Waypoint.from_plugshare(wp))
+                    except:
+                        print(wp)
+                        raise
+                leg = Leg(name=leg_data["trip_name"], gmaps_link=leg_data["maps_link"], waypoints=waypoints)
+                legs.append(leg)
+
+        return cls(name=path.stem, legs=legs)
+
+    def __str__(self) -> str:
+        text = f"{self.name}\n"
+        for l in self.legs:
+            text += '\n'.join(map(lambda x: "|-- " + x, str(l).strip().split("\n"))) + "\n"
+
+        text += f"{'-' * 91}\n"
+        text += f"{' ' * 17} | {self.distance: >7.2f} km | {hours_human_readable(self.duration)} |\n"
+        text += f"{'-' * 91}\n"
+
+        return text
+
+    @property
+    def distance(self) -> float:
+        return sum(map(lambda l: l.distance, self.legs))
+
+    @property
+    def duration(self) -> int:
+        return sum(map(lambda l: l.duration, self.legs))
+
+
+@dataclass
+class Waypoint:
+    name: str
+    type: str
+    address: str
+    distance: float
+    duration: int
+
+    @classmethod
+    def from_plugshare(cls, data: dict[str,str]) -> Waypoint:
+        type_map = {
+            "icon-M": "marker",
+            "icon-Y": "charger",
+        }
+
+        return cls(
+            name=data["display"],
+            type=type_map.get(data["icon"], "unknown"),
+            address=data["address"],
+            distance=float(data["distance"].strip().split(' ', maxsplit=1)[0]),
+            duration=sum([int(t) * (1 if i-1 == 0 else 60) for i,t in enumerate(reversed(data["duration"].strip().split(' '))) if i%2 == 1]),
+        )
+
+    def __str__(self) -> str:
+        text = (
+            f"[{self.type: >7s}] "
+            f"| {self.distance: >7.2f} km "
+            f"| {hours_human_readable(self.duration)} "
+            f"| {self.name[:40]: >43s}{'...' if len(self.name) > 40 else '   '}"
+            "\n"
+        )
+
+        return text
